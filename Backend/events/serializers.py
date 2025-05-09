da4a5d794f88dc3ff89517a35abd064aca3cd00f
OmarAlaraby <omar.alaraby23@gmail.com>
2025-05-09 16:26:56 +0300

    edited evnets serializer to include more fields (booked & image_upload)

--- Backend/events/serializers.py
+++ Backend/events/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Event, Tags
+from tickets.models import Ticket
 
 class TagsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,13 +11,25 @@ class TagsSerializer(serializers.ModelSerializer):
         
 class EventSerializer(serializers.ModelSerializer):
     tags = TagsSerializer(many=True, required=False)
+    booked = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
+    image_upload = serializers.ImageField(write_only=True, required=False)
+
     
     class Meta:
         model = Event
         fields = ['id', 'title', 'description', 'price',
-                  'venue', 'image', 'category', 'tags',
+                  'venue', 'image', 'image_upload', 'category', 'tags', 'booked',
                   'start_date', 'deadline', 'created_at']
         read_only_fields = ['id', 'created_at']
+        
+    def get_image(self, obj):
+        if obj.image :
+            return obj.image.url
+        return None
+        
+    def get_booked(self, obj):
+        return Ticket.objects.filter(user=self.context['request'].user, event=obj).exists()
         
     def create(self, validated_data):
         tags_data = validated_data.pop('tags', [])
